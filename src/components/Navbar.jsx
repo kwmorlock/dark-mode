afb7a610b0b2a909b4bde7165599c861cd62ce35
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-08 20:17:15 -0400

    stretch added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import {useDarkMode} from '../Hooks/useDarkMode';
+// import {useDarkMode} from '../Hooks/useDarkMode';
+import {Link} from 'react-router-dom';
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode(false);
+const Navbar = ({darkMode, setDarkMode}) => {
+  // const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
+      <Link to ="/thisisfine">
       <h1>Crypto Tracker</h1>
+      </Link>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
---
+++ src/components/thisisfine.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import thisisfine from '../assets/thisisfine.jpg';
+
+const Thisisfine = () => {
+return (
+    <div>
+    <img src={thisisfine}/>
+    </div>
+)
+}
+
+export default Thisisfine;
